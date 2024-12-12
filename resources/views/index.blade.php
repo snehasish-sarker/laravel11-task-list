6bc9694cbf64f7759748ad4d0cccf8603a56c49e
snehasish <snehasish.bgc@gmail.com>
2024-12-12 17:00:52 +0600

    feat: task list and show task details

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'List of task')
+
+@section('content')
+@forelse($tasks as $task)
+    <ul>
+        <li>
+            <a href="{{ route('task.show', ['id'=>$task->id]) }}">{{ $task->title }}</a>
+        </li>
+    </ul>
+
+@empty
+    <p>No List</p>
+@endforelse
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',function (){
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function ()  {
+    return view('index', [
+        'tasks' => App\Models\Task::latest()->get()
+    ]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id){
+
+    return view('show', ['task'=>App\Models\Task::findOrFail($id)]);
+})->name('task.show');
+
+Route::fallback(function (){
+    return "Are you lost?";
 });
