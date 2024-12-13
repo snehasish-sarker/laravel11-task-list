21a9604697a5161a29f3a5254a165bba601a7e48
snehasish <snehasish.bgc@gmail.com>
2024-12-13 09:10:22 +0600

    feat: keeping old value

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -15,7 +15,7 @@
             <label for="title">
                 Title
             </label>
-            <input text="text" name="title" id="title" />
+            <input text="text" name="title" id="title" value="{{ old('title') }}" />
             @error('title')
                 <div class="error-message"> {{ $message }} </div>
             @enderror
@@ -23,14 +23,14 @@
         </div>
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5"></textarea>
+            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
             @error('description')
             <div class="error-message"> {{ $message }} </div>
             @enderror
         </div>
         <div>
             <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10"></textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
             @error('long_description')
             <div class="error-message"> {{ $message }} </div>
             @enderror
