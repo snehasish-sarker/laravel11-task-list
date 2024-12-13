6b298fc7a4b54d3252d68d63c147d067bbf24396
snehasish <snehasish.bgc@gmail.com>
2024-12-13 10:22:56 +0600

    ref: edit delete link

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,7 +8,7 @@
     </style>
 @endsection
 @section('content')
-    <form method="POST" action="{{ isset($task) ? route('tasks.update', ['task'=>$task->id]) : route('tasks.store') }}">
+    <form method="POST" action="{{ isset($task) ? route('tasks.update', ['task'=>$task]) : route('tasks.store') }}">
         @csrf
         @isset($task)
             @method('PUT')
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,8 +9,8 @@
 @endif
 <p>{{ $task->created_at }}</p>
 <p>{{ $task->updated_at }}</p>
-
-<form action="{{ route('tasks.destroy', ['task'=>$task->id]) }}" method="post">
+<a href="{{ route('tasks.edit', ['task'=>$task]) }}">Edit</a>
+<form action="{{ route('tasks.destroy', ['task'=>$task]) }}" method="post">
     @csrf
     @method('DELETE')
     <button type="submit">
